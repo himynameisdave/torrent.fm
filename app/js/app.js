82ada10e532946beb21a9531dd70381ce6d24856
Dave Lunny <sir.dave@me.com>
2014-10-05 23:16:39 -0400

    added some last.fm auth stuff
    Added some last.fm auth stuff, as well as a purl scrupt to help parse
    the token from the URL. Added the actual last.fm api script, to make
    thinfs simpler. Concatted the main lastfm script and the cache and md5
    companion scripts as well.

--- app/js/app.js
+++ app/js/app.js
@@ -16,7 +16,28 @@ app.config(function($stateProvider, $urlRouterProvider) {
 
 
 app.controller('Controller', function ($scope, $http) {
+	
+	//	Set defaults shit for $http
 	$http.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded; charset=UTF-8';
+
+
+	//	Set some defualts for last.fm stuff:
+	var api_key = $scope.api_key = '0808b2872e0e3dc09d4530a90deb4418';
+
+	// 	Create a cache object
+	var cache = new LastFMCache();
+
+	// Create a LastFM object
+	var lastfm = new LastFM({
+	  apiKey    : '0808b2872e0e3dc09d4530a90deb4418',
+	  apiSecret : '17d41ed2a0967ef14bcd3338f23b8a9c',
+	  cache     : cache
+	});
+
+	//	Real callback
+	// var callbackUrl = $scope.callbackUrl = 'http://himynameisdave.github.io/torrent.fm/';
+	//	development callback
+	var callbackUrl = $scope.callbackUrl = 'http://localhost:8000/app/index.html';
 
 
 	//	Dummy data grab for testing
@@ -34,10 +55,42 @@ app.controller('Controller', function ($scope, $http) {
 	});
 
 
+	var init = function(){
+		//	get a token (if it exists);
+		var token = $.url().param('token');
+
+		if(token){
+
+	    	$scope.authorized = true;
+
+			// lastfm.auth.getSession({
+		 //    	token: token
+			// },
+			// {
+			//     success: function(data_sess) {
+			//     	console.log(data_sess);
+			//     	$scope.username = data_sess;
+			//     },
+			//     error: function(data_sess_error) {
+			//     	console.log(data_sess_error);
+			//     	$scope.authorized = false;
+			//     }
+			// });	
+			
+		}else{
+
+
+
+		}
+
+	};
+
+	$(document).ready(init);
+
+
+	$scope.authUser = function(){
+		window.location = 'http://www.last.fm/api/auth/?api_key=' + $scope.api_key + '&cb=' + $scope.callbackUrl;
+	};
 	
-
-
-
-		
 
 });
